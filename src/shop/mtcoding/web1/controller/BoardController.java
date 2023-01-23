3eb441edc9fa7c5df8692eb25df34cb4cb01e8bc
metacoding <getinthere@naver.com>
2023-01-24 05:20:18 +0900

    fd

--- src/shop/mtcoding/web1/controller/BoardController.java
+++ src/shop/mtcoding/web1/controller/BoardController.java
@@ -15,7 +15,7 @@ public class BoardController{
         return ViewResolver.resolve("/board/list");
     }
     public Board detail(int id){
-        // json 응답 234
+        // json 응답 2345
         System.out.println("detail : 요청됨");
         Board board = new Board();
         board.setId(id);
